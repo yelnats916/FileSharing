ed957b1585f7b00100459eb09e4be8207c2f2667
root <root@ip-172-31-30-159.us-west-1.compute.internal>
2015-03-06 02:39:02 +0000

    1.10 aws-sdk-core jar

--- src/S3Servlet.java
+++ src/S3Servlet.java
@@ -6,7 +6,15 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 public class S3Servlet extends HttpServlet {
 
