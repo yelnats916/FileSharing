e9289030d24a323aecc1cf373f8f51abccecc9aa
root <root@ip-172-31-30-159.us-west-1.compute.internal>
2015-03-06 14:23:38 +0000

    1.12 s3 redirect to file url

--- src/S3Servlet.java
+++ src/S3Servlet.java
@@ -12,6 +12,7 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
@@ -27,20 +28,25 @@ public class S3Servlet extends HttpServlet {
          "AKIAJK6CDWQ7EEPXP45Q", "D2auJzePUKq2AHXbsU1W3ZFtiBRcMNupKEcjfIh6");
 
       // create a client connection based on credentials
-      AmazonS3 s3client = new AmazonS3Client(credentials);
-      
+      AmazonS3 conn = new AmazonS3Client(credentials);
+
+      GeneratePresignedUrlRequest urlReq = null;
       String msgOutput = null;
-      for (Bucket bucket : s3client.listBuckets()) {
+      for (Bucket bucket : conn.listBuckets()) {
          msgOutput = bucket.getName();
+         urlReq = new GeneratePresignedUrlRequest(bucket.getName(), "stang05HolidaySchedule.pdf");
       }
       /*
       s3client.putObject(new PutObjectRequest(bucketName, fileName, 
          new File("C:\\Users\\user\\Desktop\\testvideo.mp4")));
       */
 
+
       try {
          request.setAttribute("message", msgOutput);
-         request.getRequestDispatcher("/home.jsp").forward(request, response);
+         response.sendRedirect("" + conn.generatePresignedUrl(urlReq));
+         //request.getRequestDispatcher("" + conn.generatePresignedUrl(urlReq)).forward(request,response);
+         //request.getRequestDispatcher("/home.jsp").forward(request, response);
       } catch (Exception ex) {
          throw new ServletException(ex);
       }
