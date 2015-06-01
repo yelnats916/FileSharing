e0c154a12df004c28ef3e6268a9c5ea2c4e1599a
root <root@ip-172-31-30-159.us-west-1.compute.internal>
2015-06-01 13:49:40 +0000

    1.23 cookie removed in all domains

--- src/LogoutServlet.java
+++ src/LogoutServlet.java
@@ -11,12 +11,16 @@ import java.sql.ResultSet;
 
 public class LogoutServlet extends HttpServlet {
 
+   private static String DOMAIN = ".ec2-54-153-35-253.us-west-1.compute.amazonaws.com";
+
    @Override
    protected void doGet(HttpServletRequest request, HttpServletResponse response)
    throws IOException, ServletException {
-      Cookie cookie = new Cookie("FileShareSite", "");
-      cookie.setMaxAge(0);
-      response.addCookie(cookie);
+      Cookie killMyCookie = new Cookie("FileShareSite", null);
+      killMyCookie.setMaxAge(0);
+      killMyCookie.setPath("/");            
+      killMyCookie.setDomain(DOMAIN);
+      response.addCookie(killMyCookie);
       response.sendRedirect("login.jsp");
    }
 }
